daf2b26251c01b848262c638c5dc5738dc2dd580
Subhadas2000 <91138533+Subhadas2000@users.noreply.github.com>
2021-10-01 01:37:47 +0530

    Add files via upload

---
+++ Matrix_sub.py
@@ -0,0 +1,14 @@
+matrix1 = [[17, 11, 12],
+   [13, 14, 15],
+   [16, 10, 18]]
+matrix2 = [[1, 2, 3],
+   [4, 5, 6],
+   [7, 8, 4]]
+rmatrix = [[0, 0, 0],
+   [0, 0, 0],
+   [0, 0, 0]]
+for i in range(len(matrix1)):
+    for j in range(len(matrix1[0])):
+        rmatrix[i][j] = matrix1[i][j] - matrix2[i][j]
+for r in rmatrix:
+    print(r)
