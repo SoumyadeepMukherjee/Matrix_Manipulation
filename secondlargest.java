01c1678ee66e284b293c9341678093fa4d9cca9c
Souvik <63863911+souvikg544@users.noreply.github.com>
2021-10-01 21:49:44 +0530

    Create secondlargest.java

---
+++ secondlargest.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class SecondLargestNumberInAnArray {
+   public static void main(String args[]){
+      int temp, size;
+      int array[] = {10, 20, 25, 63, 96, 57};
+      size = array.length;
+
+      for(int i = 0; i<size; i++ ){
+         for(int j = i+1; j<size; j++){
+            if(array[i]>array[j]){
+               temp = array[i];
+               array[i] = array[j];
+               array[j] = temp;
+            }
+         }
+      }
+      System.out.println("Second largest element is:: "+array[size-2]);
+      System.out.println("largest element is:: "+array[size-1]);
+     
+   }
+}
